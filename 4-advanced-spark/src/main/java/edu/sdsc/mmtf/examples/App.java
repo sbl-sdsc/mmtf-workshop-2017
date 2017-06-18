98e9b5bb417eff8c38ef5e1c4435e9cc81597cc9
Peter Rose <pwrose@ucsd.edu>
2017-06-18 12:28:27 -0700

    add folders

---
+++ 4-advanced-spark/src/main/java/edu/sdsc/mmtf/examples/App.java
@@ -0,0 +1,13 @@
+package edu.sdsc.mmtf.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
