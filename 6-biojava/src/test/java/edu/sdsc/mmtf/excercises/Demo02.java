312b56fce76cbbbb5b4f7c4eff1666416c1bbb3e
Aleix Lafita <aleixlafita@gmail.com>
2017-06-21 17:03:07 +0100

    Create Demo02

---
+++ 6-biojava/src/test/java/edu/sdsc/mmtf/excercises/Demo02.java
@@ -0,0 +1,58 @@
+package edu.sdsc.mmtf.excercises;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.biojava.nbio.structure.Structure;
+import org.biojava.nbio.structure.StructureException;
+import org.biojava.nbio.structure.align.util.AtomCache;
+import org.biojava.nbio.structure.io.FileParsingParameters;
+
+/**
+ * 
+ * Load a structure in the different formats.
+ * 
+ * @author Aleix Lafita
+ *
+ */
+public class Demo02 {
+
+	public static void main(String[] args) throws IOException, StructureException {
+		
+		// Structure loading object
+		AtomCache cache = new AtomCache();
+		
+		// File parsing parameters
+		FileParsingParameters params = cache.getFileParsingParams();
+		
+		// Select the file format to use (mmtf is the default)
+		//cache.setUseMmtf(false);
+		//cache.setUseMmCif(false);
+		
+		// Select some other parameters
+		//params.setParseSecStruc(true);
+		
+		// Print some of the parameters to check them
+		System.out.println("Path: " + cache.getCachePath());
+		System.out.println("Using MMTF: " + cache.isUseMmtf());
+		System.out.println("Parse SS: " + params.isParseSecStruc());
+		System.out.println("Maximum atoms: " + params.getMaxAtoms());
+		
+		// Measure the parsing time
+		long start = System.nanoTime();
+		
+		// Download the lysozyme structure 148L
+		Structure lysozyme = cache.getStructure("148L");
+		
+		System.out.println("Parsing time: " + (System.nanoTime() - start) / 1000000 + " ms");
+		
+		//System.out.println(lysozyme);
+		
+		try(PrintWriter out = new PrintWriter("/Users/aleixlafita/Downloads/148l_biojava.pdb")){
+		    out.println(lysozyme.toPDB());
+		    out.close();
+		}
+
+	}
+
+}
