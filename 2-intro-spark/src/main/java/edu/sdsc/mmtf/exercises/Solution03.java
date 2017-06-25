8fbb0f8d5d056ea00ac46c89e53c96cf5798295b
Yana Valasatava <yana.valasatava@rcsb.org>
2017-06-24 20:01:25 -0700

    Filter JavaRDD example

---
+++ 2-intro-spark/src/main/java/edu/sdsc/mmtf/exercises/Solution03.java
@@ -0,0 +1,31 @@
+package edu.sdsc.mmtf.exercises;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+
+public class Solution03 {
+
+	/** Filter example: get even numbers from a sequence of integers ranging from 1 to 10
+	 * 
+	 * @author Yana Valasatava
+	 */
+
+	public static void main(String[] args) {
+
+		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName(Solution03.class.getSimpleName());
+		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		// Parallelized with 2 partitions
+		JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+				2);
+        // TODO 
+		// Get only even numbers from a sequence of integers ranging from 1 to 10
+
+		rdd.filter( e -> e % 2 == 0 ).foreach(e -> System.out.println(e));
+
+		sc.close();
+	}
+}
